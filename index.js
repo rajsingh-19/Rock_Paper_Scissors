c86d83e3163170c8d14180c292690c1719d803fa
Raj Singh <rajsingh.19.vns@gmail.com>
2024-09-21 23:10:09 +0530

    Developed Celebrate page UI and implemented toggling feature in rules button

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 let rulesContainer = document.getElementById("rulesContainer");
 
-
 let rulesToggling = () => {
     rulesContainer.classList.toggle("hide");
 } 
+let closeRules = () => {
+    rulesContainer.classList.add("hide");
+}
+let goToHomePage = () => {
+    window.location.href = "./index.html";
+}
